afdd368c8a4efc11a8dccd8281356cc658be75d9
tiana <tianaaprianti85@gmail.com>
2022-11-20 07:25:18 +0700

    second change

--- project/src/component/Navbar.js
+++ project/src/component/Navbar.js
@@ -3,27 +3,27 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-dark py-3 sticky-top">
+    <nav className="navbar navbar-expand-lg bg-light-pink py-3 sticky-top">
         <div className="container-fluid">
             <ul className='navbar-nav'>
                 <li className='nav-item me-5 ms-5'>
-                    <a className="navbar-brand" href="#">
-                    <Link to="/">
-                        <img src={Logo} height="20px"></img>
-                    </Link>
+                    <a className="navbar-brand Heading" href="#">
+                    <a to="/">
+                        <img  height="20px"></img>
+                    </a>
                         
                     </a>
                 </li>
                 <li className='nav-item me-3'>
-                    <Link to="/" className='nav-link active'>
-                        <FaHome/>
-                    </Link>
+                    <a to="/" className='nav-link active'>
+                        <i class="bi bi-house-fill fs-4"></i>
+                    </a>
                    
                 </li>
             </ul>
             <form className="d-flex input-group" role="search">
                 <input className="form-control col-auto" type="search" placeholder="Ask Question Here" aria-label="Recipient's username" aria-describedby="button-addon3"/>
-                <Link className="btn btn-light btn-outline-secondary" type="button" id="button-addon3" to="/result">Search</Link>
+                <a className="btn btn-light btn-outline-secondary" type="button" id="button-addon3" to="/result">Search</a>
             </form>
             <button 
             className="navbar-toggler" 
@@ -36,16 +36,16 @@ function Navbar() {
             <div className="col-3 collapse navbar-collapse justify-content-end" id="#navMenu">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <Link to="/aboutus" className='nav-link active'>
+                        <a to="/aboutus" className='nav-link active'>
                             About Us
-                        </Link>
+                        </a>
     
                     </li>
                     <li className="nav-item">
-                        <Link to="/loginform" className="nav-link active">Log In</Link>
+                        <a to="/loginform" className="nav-link active">Log In</a>
                     </li>
                     <li className="nav-item">
-                        <Link to="/signinform" className="nav-link active">Sign In</Link>
+                        <a to="/signinform" className="nav-link active">Sign In</a>
                     </li>
                 </ul>
             </div>
